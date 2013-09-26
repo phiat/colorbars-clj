d2fcf97defad29b38bdea9e9afcfcda1c9e9a6c6
Patrick Ryan <phiat99@gmail.com>
2013-09-26 04:22:08 -0400

    updated project.clj: description, url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject color-test "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Clojure library designed to make rpg-style stat bars, using clansi for coloring."
+  :url "https://github.com/phiat/colorbars-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
